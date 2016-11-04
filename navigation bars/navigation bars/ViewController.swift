7c3571ec2f7da8c94401acab1c227d4226687572
Bryan Alexander <calbryan@gmail.com>
2016-11-04 12:38:47 -0700

    completed navigation bar exercise

---
+++ navigation bars/navigation bars/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  navigation bars
+//
+//  Created by Bryan Alexander on 11/3/16.
+//  Copyright © 2016 sarva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        print ("button pressed");
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
