60c0ea37bf59b4b2a6be1070a42cdc6589b766ca
Bryan Alexander <calbryan@gmail.com>
2016-11-04 16:24:35 -0700

    completed multiple view controllers exercise

---
+++ multiple view controllers/multiple view controllers/SecondViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SecondViewController.swift
+//  multiple view controllers
+//
+//  Created by Bryan Alexander on 11/4/16.
+//  Copyright © 2016 sarva. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        print("Second view controller loaded")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
